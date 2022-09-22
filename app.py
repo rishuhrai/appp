b2865d5a71bb6cbccf369098b88fd72d73363e48
Rishu <rishuhrai@gmail.com>
2022-09-22 18:42:12 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+
+import numpy as np
+from flask import Flask, request
+from flask_cors import CORS
+from keras.models import load_model
+from PIL import Image, ImageOps
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    pass
